5d2198dd44d8a4c3fdb2adaf65c18a9306158296
Daniel Eagle <daniel@danieleagle.com>
2020-11-25 11:25:07 -0600

    [BREAKING] [FEATURE] Added Veracode Pipeline Scanner

--- src/com/danieleagle/GetchaJenLib/agents/VeracodeAgent.groovy
+++ src/com/danieleagle/GetchaJenLib/agents/VeracodeAgent.groovy
@@ -27,7 +27,9 @@ class VeracodeAgent implements Serializable {
   }
 
   /**
-   * Scans the workspace using the Veracode scanner.
+   * Scans the workspace using the Veracode Policy Scanner. A policy scan is a total analysis of your compiled
+   * binary/zipped code as it pertains to a security policy within the Veracode platform, matched against existing flaw
+   * data and utilized for platform analytics across your application program.
    * @param veracodeCredId The credential ID for Veracode.
    * @param scanOptions The scan options. Keys and values should be Strings. Required keys are appName, canFailJob,
    *                    criticality, debug, sandboxName, scanName, timeout, uploadIncludesPattern, and waitForScan.
@@ -35,7 +37,7 @@ class VeracodeAgent implements Serializable {
    *                    and uploadExcludesPattern.
    * @throws IllegalArgumentException when passing an empty or null argument.
    */
-  void scan(final String veracodeCredId, final Map scanOptions) throws IllegalArgumentException {
+  void runPolicyScan(final String veracodeCredId, final Map scanOptions) throws IllegalArgumentException {
     if (veracodeCredId && scanOptions && scanOptions.get("applicationName")
         && scanOptions.get("applicationName") instanceof String && scanOptions.get("canFailJob")
         && scanOptions.get("canFailJob") instanceof String
@@ -62,20 +64,56 @@ class VeracodeAgent implements Serializable {
         && scanOptions.get("uploadExcludesPattern") instanceof String) ? scanOptions.get("uploadExcludesPattern") : ""
 
       steps.withCredentials([steps.usernamePassword(credentialsId: veracodeCredId,
-            usernameVariable: "veracodeUserName", passwordVariable: "veracodePassword")]) {
-          steps.veracode(applicationName: scanOptions.get("applicationName"),
-            canFailJob: scanOptions.get("canFailJob").toBoolean(), criticality: scanOptions.get("criticality"),
-            debug: scanOptions.get("debug").toBoolean(), fileNamePattern: fileNamePattern,
-            replacementPattern: replacementPattern, sandboxName: scanOptions.get("sandboxName"),
-            scanExcludesPattern: scanExcludesPattern, scanIncludesPattern: scanIncludesPattern,
-            scanName: scanOptions.get("scanName"), teams: scanOptions.get("teams"),
-            timeout: scanOptions.get("timeout").toInteger(), uploadExcludesPattern: uploadExcludesPattern,
-            uploadIncludesPattern: scanOptions.get("uploadIncludesPattern"), vid: steps.veracodeUserName,
-            vkey: steps.veracodePassword, waitForScan: scanOptions.get("waitForScan").toBoolean())
+          usernameVariable: "veracodeUserName", passwordVariable: "veracodePassword")]) {
+        steps.veracode(applicationName: scanOptions.get("applicationName"),
+          canFailJob: scanOptions.get("canFailJob").toBoolean(), criticality: scanOptions.get("criticality"),
+          debug: scanOptions.get("debug").toBoolean(), fileNamePattern: fileNamePattern,
+          replacementPattern: replacementPattern, sandboxName: scanOptions.get("sandboxName"),
+          scanExcludesPattern: scanExcludesPattern, scanIncludesPattern: scanIncludesPattern,
+          scanName: scanOptions.get("scanName"), teams: scanOptions.get("teams"),
+          timeout: scanOptions.get("timeout").toInteger(), uploadExcludesPattern: uploadExcludesPattern,
+          uploadIncludesPattern: scanOptions.get("uploadIncludesPattern"), vid: steps.veracodeUserName,
+          vkey: steps.veracodePassword, waitForScan: scanOptions.get("waitForScan").toBoolean())
       }
     } else {
-      throw new IllegalArgumentException("The argument passed to the VeracodeAgent scan method is invalid. It could be " +
-        "empty or null.") as Throwable
+      throw new IllegalArgumentException("The argument passed to the VeracodeAgent runPolicyScan method is invalid. It " +
+        "could be empty or null.") as Throwable
+    }
+  }
+
+  /**
+  * Scans the workspace using the Veracode Pipeline Scanner. A pipeline scan uses a lightweight jar file that uploads
+  * your compiled/zipped code to the Veracode platform where it will fully scan your application. As part of this upload
+  * you can tell the scan to fail based on CWEs or specific severities such as Medium flaws and above. It’s meant for
+  * a quick YES/NO answer to whether you should push your code through whether it be to commit to an initial branch
+  * or with deployment.
+  * @param veracodeCredId The credential ID for Veracode.
+  * @param scanOptions The scan options. Keys and values should be Strings. Required keys are filePath, failOnSeverity,
+  *                    failOnCwe, timeout, and applicationName.
+  * @throws IllegalArgumentException when passing an empty or null argument.
+  */
+  void runPipelineScan(final String veracodeCredId, final Map scanOptions) throws IllegalArgumentException {
+    if (veracodeCredId && scanOptions && scanOptions.get("filePath") && scanOptions.get("filePath") instanceof String
+        && scanOptions.get("failOnSeverity") && scanOptions.get("failOnSeverity") instanceof String
+        && scanOptions.get("failOnCwe") && scanOptions.get("failOnCwe") instanceof String && scanOptions.get("timeout")
+        && scanOptions.get("timeout") instanceof String && scanOptions.get("applicationName")
+        && scanOptions.get("applicationName") instanceof String) {
+      steps.withCredentials([steps.usernamePassword(credentialsId: veracodeCredId,
+          usernameVariable: "veracodeUserName", passwordVariable: "veracodePassword")]) {
+        steps.sh "curl -O https://downloads.veracode.com/securityscan/pipeline-scan-LATEST.zip"
+        steps.sh "unzip pipeline-scan-LATEST.zip pipeline-scan.jar"
+        steps.sh "java -jar pipeline-scan.jar --veracode_api_id \"${steps.veracodeUserName}\" " +
+          "--veracode_api_key \"${steps.veracodePassword}\" " +
+          "--file \"${scanOptions.get('filePath')}\" " +
+          "--fail_on_severity=\"${scanOptions.get('failOnSeverity')}\" " +
+          "--fail_on_cwe=\"${scanOptions.get('failOnCwe')}\" " +
+          "--timeout \"${scanOptions.get('timeout')}\" " +
+          "--issue_details true "
+          "--project_name \"${scanOptions.get('applicationName')}\""
+      }
+    } else {
+      throw new IllegalArgumentException("The argument passed to the VeracodeAgent runPipelineScan method is invalid. It " +
+        "could be empty or null.") as Throwable
     }
   }
 }
--- vars/veracodeScanner.groovy
+++ vars/veracodeScanner.groovy
@@ -4,7 +4,9 @@ import com.danieleagle.GetchaJenLib.agents.VeracodeAgent
 import com.danieleagle.GetchaJenLib.exceptions.GenericToolsException
 
 /**
- * Invokes the Veracode scanner to scan the code.
+ * Scans the workspace using the Veracode Policy Scanner. A policy scan is a total analysis of your compiled
+ * binary/zipped code as it pertains to a security policy within the Veracode platform, matched against existing flaw
+ * data and utilized for platform analytics across your application program.
  * @param veracodeCredId The credential ID for Veracode.
  * @param scanOptions The scan options. Keys and values should be Strings. Required keys are appName, canFailJob,
  *                    criticality, debug, sandboxName, scanName, timeout, uploadIncludesPattern, and waitForScan.
@@ -13,7 +15,7 @@ import com.danieleagle.GetchaJenLib.exceptions.GenericToolsException
  * @throws IllegalArgumentException when passing an empty or null argument.
  * @throws GenericToolsException when the Veracode plugin returns an error.
  */
-void invoke(final String veracodeCredId, final Map scanOptions) throws IllegalArgumentException, GenericToolsException {
+void runPolicy(final String veracodeCredId, final Map scanOptions) throws IllegalArgumentException, GenericToolsException {
   if (veracodeCredId && scanOptions && scanOptions.get("applicationName")
       && scanOptions.get("applicationName") instanceof String && scanOptions.get("canFailJob")
       && scanOptions.get("canFailJob") instanceof String
@@ -28,14 +30,38 @@ void invoke(final String veracodeCredId, final Map scanOptions) throws IllegalAr
       && scanOptions.get("uploadIncludesPattern") && scanOptions.get("uploadIncludesPattern") instanceof String
       && scanOptions.get("waitForScan") && scanOptions.get("waitForScan") instanceof String
       && (scanOptions.get("waitForScan") == "true" || scanOptions.get("waitForScan") == "false")) {
-    new VeracodeAgent(this).scan(veracodeCredId, scanOptions)
+    new VeracodeAgent(this).runPolicyScan(veracodeCredId, scanOptions)
 
     // check the build log immediately after the Veracode plugin is invoked since when it fails, it won't throw an exception
     if (currentBuild.rawBuild.log.contains("Error- Returned code from wrapper:")) {
       throw new GenericToolsException("The Veracode plugin failed.") as Throwable
     }
   } else {
-    throw new IllegalArgumentException("The argument passed to the veracodeScanner.invoke step is invalid. It could be " +
+    throw new IllegalArgumentException("The argument passed to the veracodeScanner.runPolicy step is invalid. It could be " +
       "empty or null.") as Throwable
+  }
+}
+
+/**
+* Scans the workspace using the Veracode Pipeline Scanner. A pipeline scan uses a lightweight jar file that uploads
+* your compiled/zipped code to the Veracode platform where it will fully scan your application. As part of this upload
+* you can tell the scan to fail based on CWEs or specific severities such as Medium flaws and above. It’s meant for
+* a quick YES/NO answer to whether you should push your code through whether it be to commit to an initial branch
+* or with deployment.
+* @param veracodeCredId The credential ID for Veracode.
+* @param scanOptions The scan options. Keys and values should be Strings. Required keys are filePath, failOnSeverity,
+*                    failOnCwe, timeout, and applicationName.
+* @throws IllegalArgumentException when passing an empty or null argument.
+*/
+void runPipeline(final String veracodeCredId, final Map scanOptions) throws IllegalArgumentException {
+  if (veracodeCredId && scanOptions && scanOptions.get("filePath") && scanOptions.get("filePath") instanceof String
+      && scanOptions.get("failOnSeverity") && scanOptions.get("failOnSeverity") instanceof String
+      && scanOptions.get("failOnCwe") && scanOptions.get("failOnCwe") instanceof String && scanOptions.get("timeout")
+      && scanOptions.get("timeout") instanceof String && scanOptions.get("applicationName")
+      && scanOptions.get("applicationName") instanceof String) {
+    new VeracodeAgent(this).runPipelineScan(veracodeCredId, scanOptions)
+  } else {
+    throw new IllegalArgumentException("The argument passed to the veracodeScanner.runPipeline step is invalid. It " +
+      "could be empty or null.") as Throwable
   }
 }
